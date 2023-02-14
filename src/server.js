6ebc7a595983e0c7c6eeedc6b768c4087d1fa2ec
anastasia <anyasirman@outlook.com>
2023-02-13 20:09:16 -0500

    added dockerfile and created image and contianer

--- src/server.js
+++ src/server.js
@@ -5,6 +5,8 @@ app.get('/', (req, res)=> {
     return res.send('HI')
 })
 
-app.listen( 3000,()=> {
+const port = process.env.PORT || 8080
+
+app.listen( port,()=> {
     console.log('running')
 })
